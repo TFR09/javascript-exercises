f117d537b6074be453c4957ee69cb844aaf2de05
Trevor <troferns040@gmail.com>
2023-09-07 23:11:36 +0400

    7 exercises done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...vals) {
 
+    for (let val of vals) {
+        let index = arr.indexOf(val);
+        if (index > -1) arr.splice(index, 1);
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let startValid = typeof(start) === "number" && (start > 0);
+    let endValid = typeof(end) === "number" && (end > 0);
 
+    if (!(startValid && endValid)) return "ERROR";
+
+    if (start > end) [start, end] = [end, start];
+
+    let sum = 0;
+    for (;start <= end; start++) {
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
